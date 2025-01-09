20feb338fc561ff0ae978d4d41c9a9ebbb706cf6
marisolkarina <marisol.kari.20@gmail.com>
2025-01-09 11:24:58 -0500

    mostrar productos en vista home.bhs

--- src/routes/viewRouter.js
+++ src/routes/viewRouter.js
@@ -1,12 +1,23 @@
 import { Router } from "express";
+import { productManager } from '../managers/productManager.js';
 
 const router = Router();
-router.get('/realTimeProducts', (req, res) => {
-    res.render('realTimeProducts', {});
+
+router.get('/', async (req, res, next) => {
+    try {
+        const productos = await productManager.getProducts();
+        res.render('home', {
+            title: 'Productos Medilook',
+            prods: productos,
+            hayProductos: productos.length !== 0
+        });
+    } catch (err) {
+        next(err);
+    }
 });
 
-router.get('/', (req, res) => {
-    res.render('home', {});
+router.get('/realTimeProducts', (req, res) => {
+    res.render('realTimeProducts', {});
 });
 
 export default router;
